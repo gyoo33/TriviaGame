9509492f6b484e8a02aa94b0b5078cd69c3bb3d8
Grace Yoo <graceyoo33@gmail.com>
2018-07-28 23:08:19 -0400

    running game commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         incorrectAnswerCount: 0,
         unansweredCount: 0,
     }
-    var timer = 101;
+    var timer = 100;
     var intervalId;
 
     var allQuestions = [{
@@ -55,141 +55,124 @@ $(document).ready(function() {
 
     // Starts the game, hides the start button when clicked, starts the timer
     function startGame () {
-        $("#startButton").on("click", function() {
-            $("#startButton").hide();
-            console.log("HI");
-            countDown();
-            for (var i = 0; i < allQuestions.length; i++) {
-                $("#gameRow").append("<h5>" + allQuestions[i].question + "</h5>");
-                for (var a = 0; a < allQuestions[i].answers.length; a++) {
-                    $("#gameRow").append("<input type='radio' name='question-" + i + "value='" + allQuestions[i].answers[a] + "'>" + allQuestions[i].answers[a]);
-                }
+        $("#startButton").remove();
+        //console.log("HI");
+        for (var i = 0; i < allQuestions.length; i++) {
+            $("#gameRow").append("<h2>" + allQuestions[i].question + "</h2>");
+            for (var a = 0; a < allQuestions[i].answers.length; a++) {
+                $("#gameRow").append("<input type='radio' name='question-" + i + "' value='" + allQuestions[i].answers[a] + "'>" + allQuestions[i].answers[a]);
             }
-            $("#gameRow").append("<button type='button' class='btn btn-dark' id='doneButton'>DONE!</button>");
-        });
-        
+        }
+        $("#gameRow").append("<button type='button' class='btn btn-dark' id='doneButton'> DONE!</button>");
     }
-    // startGame();
+
 
     // Clears the intervalId, preventing multiple instances
     function startCountDown () {
         clearInterval(intervalId);
         intervalId = setInterval(countDown, 1000);
         startGame();
+        $("#doneButton").on("click", stopCountDown);
     }
 
     // Reduces the timer in 1 second increments while dynamically updating webpage to notify user of remaining time, when timer reaches 0 user 
     function countDown () {
         timer--;
-        $("#buttonRow").html("<h2>Time Remaining: " + timer + " Seconds</h2>");
-        console.log(timer);
+        $("#buttonRow").html("<h2>Time Remaining: " + timer + " Seconds </h2>");
+        //console.log(timer);
         if (timer === 0) {
             stopCountDown();
-            console.log("DONE");
+            //console.log("DONE");
         }
     }
 
     function stopCountDown () {
         clearInterval(intervalId);
-        for (var i = 0; i < 10; i++) {
-            $.each($("input[name='allQuestions-0']:checked"), function() {
+        //console.log("hi");
+            $.each($("input[name='question-0']:checked"), function() {
                 if ($(this).val() == allQuestions[0].correctA) {
                     answerCount.correctAnswerCount++;
                 } else if ($(this).val() !== allQuestions[0].correctA) {
                     answerCount.incorrectAnswerCount++;
-                } else {
-                    answerCount.unansweredCount++;
                 }
             })
-            $.each($("input[name='allQuestions-1']:checked"), function() {
+            $.each($("input[name='question-1']:checked"), function() {
                 if ($(this).val() == allQuestions[1].correctA) {
                     answerCount.correctAnswerCount++;
                 } else if ($(this).val() !== allQuestions[1].correctA) {
                     answerCount.incorrectAnswerCount++;
-                } else {
-                    answerCount.unansweredCount++;
                 }
             })
-            $.each($("input[name='allQuestions-2']:checked"), function() {
+            $.each($("input[name='question-2']:checked"), function() {
                 if ($(this).val() == allQuestions[2].correctA) {
                     answerCount.correctAnswerCount++;
                 } else if ($(this).val() !== allQuestions[2].correctA) {
                     answerCount.incorrectAnswerCount++;
-                } else {
-                    answerCount.unansweredCount++;
                 }
             })
-            $.each($("input[name='allQuestions-3']:checked"), function() {
+            $.each($("input[name='question-3']:checked"), function() {
                 if ($(this).val() == allQuestions[3].correctA) {
                     answerCount.correctAnswerCount++;
                 } else if ($(this).val() !== allQuestions[3].correctA) {
                     answerCount.incorrectAnswerCount++;
-                } else {
-                    answerCount.unansweredCount++;
                 }
             })
-            $.each($("input[name='allQuestions-4']:checked"), function() {
+            $.each($("input[name='question-4']:checked"), function() {
                 if ($(this).val() == allQuestions[4].correctA) {
                     answerCount.correctAnswerCount++;
                 } else if ($(this).val() !== allQuestions[4].correctA) {
                     answerCount.incorrectAnswerCount++;
-                } else {
-                    answerCount.unansweredCount++;
                 }
             })
-            $.each($("input[name='allQuestions-5']:checked"), function() {
+            $.each($("input[name='question-5']:checked"), function() {
                 if ($(this).val() == allQuestions[5].correctA) {
                     answerCount.correctAnswerCount++;
                 } else if ($(this).val() !== allQuestions[5].correctA) {
                     answerCount.incorrectAnswerCount++;
-                } else {
-                    answerCount.unansweredCount++;
                 }
             })
-            $.each($("input[name='allQuestions-6']:checked"), function() {
+            $.each($("input[name='question-6']:checked"), function() {
                 if ($(this).val() == allQuestions[6].correctA) {
                     answerCount.correctAnswerCount++;
                 } else if ($(this).val() !== allQuestions[6].correctA) {
                     answerCount.incorrectAnswerCount++;
-                } else {
-                    answerCount.unansweredCount++;
                 }
             })
-            $.each($("input[name='allQuestions-7']:checked"), function() {
+            $.each($("input[name='question-7']:checked"), function() {
                 if ($(this).val() == allQuestions[7].correctA) {
                     answerCount.correctAnswerCount++;
                 } else if ($(this).val() !== allQuestions[7].correctA) {
                     answerCount.incorrectAnswerCount++;
-                } else {
-                    answerCount.unansweredCount++;
                 }
             })
-            $.each($("input[name='allQuestions-8']:checked"), function() {
+            $.each($("input[name='question-8']:checked"), function() {
                 if ($(this).val() == allQuestions[8].correctA) {
                     answerCount.correctAnswerCount++;
                 } else if ($(this).val() !== allQuestions[8].correctA) {
                     answerCount.incorrectAnswerCount++;
-                } else {
-                    answerCount.unansweredCount++;
                 }
             })
-            $.each($("input[name='allQuestions-9']:checked"), function() {
+            $.each($("input[name='question-9']:checked"), function() {
                 if ($(this).val() == allQuestions[9].correctA) {
                     answerCount.correctAnswerCount++;
                 } else if ($(this).val() !== allQuestions[9].correctA) {
                     answerCount.incorrectAnswerCount++;
-                } else {
-                    answerCount.unansweredCount++;
                 }
-            })
-        };
-        $("#gameRow").hide();
-        $("<div>").html('<h2>All Done!</h2>');
-        $("<div>").html('<h3>Correct Answers: ' + answerCount.correctAnswerCount + '</h3>');
-        $("<div>").html('<h3>Incorrect Answers: ' + answerCount.incorrectAnswerCount + '</h3>');
-        $("<div>").html('<h3>Unanswered: ' + answerCount.unansweredCount + '</h3>');
+            });
+        answerCount.unansweredCount = 10 - answerCount.incorrectAnswerCount - answerCount.correctAnswerCount;
+
+        //console.log(answerCount.incorrectAnswerCount);
+        results();    
+    };
+
+    function results () {
+        $("#gameRow h2").remove();
+        $("#gameRow").html("<h2>All Done!</h2>");
+        $("#gameRow").append('<h3>Correct Answers: ' + answerCount.correctAnswerCount + '</h3>');
+        $("#gameRow").append('<h3>Incorrect Answers: ' + answerCount.incorrectAnswerCount + '</h3');     
+        $("#gameRow").append('<h3>Unanswered: ' + answerCount.unansweredCount + '</h3>');
     }
+
     $("#startButton").on("click", startCountDown);
-    $("#doneButton").on("click", stopCountDown);
 
 });
